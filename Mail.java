dec6a76162cf2790ce64f67e5c9abbbffdcd3ea7
Gouri Rajesh <64693139+GouriRajesh@users.noreply.github.com>
2020-08-25 16:39:41 +0530

    Create Mail.java

---
+++ Mail.java
@@ -0,0 +1,84 @@
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+class Mail{
+		
+	Properties properties;
+	MimeMessage message;
+	String to;
+	String subject;
+	public void send(String to,String mes){
+
+			this.to = to;
+			String from = "fromemail"; 
+			properties = new  Properties();
+			properties.put("mail.smtp.auth","true");
+			properties.put("mail.smtp.starttls.enable","true");
+			properties.put("mail.smtp.host", "smtp.gmail.com");
+			properties.put("mail.smtp.port",587);
+
+			
+			try
+			{  
+				Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator()
+				{
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("youremailid","yourpassword");
+					}
+				});
+				
+				message = new MimeMessage(session); 
+				message.setFrom(new InternetAddress(from));  
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+				message.setSubject("GREETINGS FROM CAREERBOOK");
+				message.setText(mes); 
+				Transport.send(message); 
+				System.out.println("Confirmation mail successfully sent"); 
+			
+			} catch (MessagingException mex) 
+			{
+				mex.printStackTrace(); 
+			} 
+		}
+	public void send(String to,String mes,String sub){
+
+		this.to = to;
+		this.subject=sub;
+		String from = "fromemail"; 
+		properties = new  Properties();
+		properties.put("mail.smtp.auth","true");
+		properties.put("mail.smtp.starttls.enable","true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port",587);
+
+		
+		try
+		{  
+			Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator()
+			{
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("youremailid","yourpassword");
+				}
+			});
+			
+			message = new MimeMessage(session); 
+			message.setFrom(new InternetAddress(from));  
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject(subject);
+			message.setText(mes); 
+			Transport.send(message); 
+			System.out.println("Mail successfully sent"); 
+		
+		} catch (MessagingException mex) 
+		{
+			mex.printStackTrace(); 
+		} 
+	}
+	
+}
